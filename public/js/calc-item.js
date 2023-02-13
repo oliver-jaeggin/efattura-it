fcc39a5ece14d18d4da5a13749829e6be7f2b784
Oliver Jaeggin <web@monkey-man.ch>
2023-02-13 11:59:36 +0100

    Fix digits after comma for quantity

--- public/js/calc-item.js
+++ public/js/calc-item.js
@@ -7,6 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   formCreateItem.querySelectorAll('input[type="number"]').forEach(inputField => {
     inputField.addEventListener('change', () => {
+      // fix quantity to two digits after comma
+      itemQty.value = parseFloat(itemQty.value).toFixed(2);
       let valueTotalItem = parseFloat(itemQty.value * itemPrice.value);
       totalItem.value = window.roundValue(valueTotalItem);
     });
